2ceff8a11a2676c6b04e11fc98b7f5011e73fd3d
Dark_Furia <robinhood010501@gmail.com>
2018-10-06 12:56:38 +0300

    Wrong value of G

--- src/settings.h
+++ src/settings.h
@@ -1,8 +1,8 @@
 // model parameters
-#define N_BODYS 1024
+#define N_BODYS 128
 #define DELTA_T 1 // seconds
-#define THREADS_AMOUNT 1024
+#define THREADS_AMOUNT 128
 
 // CONSTANTS
-#define G 6.67e-13
+#define G 6.67e-11
 #define EPSILON 1 // helps to avoid singularity
